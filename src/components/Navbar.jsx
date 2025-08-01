3c54755b3f76b393dcff550d4e3601758fc33ed3
Parth <140729488+thisisparth187@users.noreply.github.com>
2025-08-01 19:25:09 +0530

    feat: navbar animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css'
 import logo from '../assets/images/logo-only-nobg.png'
 const Navbar = () => {
+    const linkClass = ({ isActive }) =>
+    isActive ? 'text-[#C29D69]' : 'hover:text-[#A27B5C]';
   return (
     <div className='navbar flex'>
-      <div className="logo w-20 flex content-center items-center text-xl text-white font-bold">
+      <div className="logo w-20 text-2xl text-white font-bold">
+        <NavLink to="/" className="flex content-center items-center">
         <img src={logo} alt="logo" className='p-4' />
-        <span>SAPPHIRE</span>
+        <span>
+            SAPPHIRE
+          </span>
+          </NavLink>
       </div>
       <ul className="nav-items flex list-none gap-30 text-white font-bold">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <NavLink to="/" className={linkClass}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={linkClass}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={linkClass}>
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </div>
   )
