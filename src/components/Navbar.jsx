e696a954130cb603192c8f519a8a1da8869dd96e
Parth <140729488+thisisparth187@users.noreply.github.com>
2025-08-01 19:33:11 +0530

    feat: navbar dropmenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,36 +4,36 @@ import '../styles/navbar.css'
 import logo from '../assets/images/logo-only-nobg.png'
 const Navbar = () => {
     const linkClass = ({ isActive }) =>
-    isActive ? 'text-[#C29D69]' : 'hover:text-[#A27B5C]';
-  return (
-    <div className='navbar flex'>
-      <div className="logo w-20 text-2xl text-white font-bold">
-        <NavLink to="/" className="flex content-center items-center">
-        <img src={logo} alt="logo" className='p-4' />
-        <span>
-            SAPPHIRE
-          </span>
-          </NavLink>
-      </div>
-      <ul className="nav-items flex list-none gap-30 text-white font-bold">
-        <li>
-          <NavLink to="/" className={linkClass}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" className={linkClass}>
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" className={linkClass}>
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-  )
+        `duration-200 ${isActive ? 'text-[#C29D69]' : 'hover:text-[#A27B5C]'}`;
+    return (
+        <div className='navbar flex'>
+            <div className="logo w-20 text-2xl text-white font-bold">
+                <NavLink to="/" className="flex content-center items-center">
+                    <img src={logo} alt="logo" className='p-4' />
+                    <span>
+                        SAPPHIRE
+                    </span>
+                </NavLink>
+            </div>
+            <ul className="nav-items flex list-none gap-30 text-white font-bold">
+                <li>
+                    <NavLink to="/" className={linkClass}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={linkClass}>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" className={linkClass}>
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
