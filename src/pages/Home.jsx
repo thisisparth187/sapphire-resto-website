69ed8d793f90d72d1837c8114469b0992696c285
Parth <140729488+thisisparth187@users.noreply.github.com>
2025-08-03 18:21:37 +0530

    added some cards on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../styles/Home.css";
+import Card from '../components/Card';
 const Home = () => {
   return (
     <div className='main'>
@@ -8,7 +9,12 @@ const Home = () => {
           <p>LUXURY HOTELS AND RESORTS</p>
           <h1>The Best Luxury Hotels <br /> in PUNE</h1>
         </div>
-        <div className="other"></div>
+        <div className="other flex justify-between">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   )
