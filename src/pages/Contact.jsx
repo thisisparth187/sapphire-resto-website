86ac3eb721c5a270ea2f96bf9a985bb479bbc7a0
aykul96 <161322465+aykul96@users.noreply.github.com>
2025-08-05 01:35:20 +0530

    Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from "react";
+import "../styles/Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
-  )
-}
+    <div div className="scroll-container">
+      <div className="card animate-card">
+        <div className="message animate-message">
+          <h1 className="message1">Want to get in touch?</h1>
+          Email us right away!
+        </div>
+        <form className="form animate-form">
+          <input
+            type="text"
+            spellCheck="false"
+            name="ContactName"
+            id="contactname"
+            className="inputbox animate-input input1"
+            placeholder="Enter your name"
+          />
+          <input
+            type="email"
+            spellCheck="false"
+            name="ContactEmail"
+            id="contactemail"
+            className="inputbox animate-input input2"
+            placeholder="Enter your e-mail"
+          />
+          <textarea
+            name="ContactMessage"
+            id="contactmessage"
+            className="inputbox textarea animate-input input3"
+            placeholder="Enter your message here"
+          />
+          <button className="submit-btn animate-input input4">Submit</button>
+        </form>
+      </div>
 
-export default Contact
+      <div className="socials">
+        <a href="http://instagram.com" target="_blank" rel="noreferrer">
+          <img
+            className="icons"
+            src="src/assets/images/instaicon.png"
+            alt="Instagram icon"
+          />{" "}
+          /sapphire_hotels
+        </a>
+        <a href="http://facebook.com" target="_blank" rel="noreferrer">
+          <img
+            className="icons"
+            src="src/assets/images/facebookicon.png"
+            alt="Facebook icon"
+          />{" "}
+          /Sapphire Hotels
+        </a>
+        <a href="http://linkedin.com" target="_blank" rel="noreferrer">
+          <img
+            className="icons"
+            src="src/assets/images/linkdinicon.png"
+            alt="LinkedIn icon"
+          />{" "}
+          /Sapphire Agency
+        </a>
+        <a href="http://x.com" target="_blank" rel="noreferrer">
+          <img
+            src="src/assets/images/xicon.png"
+            alt="X icon"
+            className="icons"
+          />{" "}
+          /@sapphirehotels
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
